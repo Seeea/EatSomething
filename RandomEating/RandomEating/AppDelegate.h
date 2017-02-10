b35b22943a348c1187cc6ee1a41b707f5ae558bc
月夜雨墨 <seeea@seeeademacbook-pro.local>
2017-02-10 14:55:47 +0800

    外形

---
+++ RandomEating/RandomEating/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomEating
+//
+//  Created by Seeea on 17/2/10.
+//  Copyright © 2017年 Seeea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomEating/RandomEating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomEating
+//
+//  Created by Seeea on 17/2/10.
+//  Copyright © 2017年 Seeea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
